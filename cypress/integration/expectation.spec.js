511f19811af70b3ba554f97b95ec7859014cf96c
Tony Spataro <xeger@xeger.net>
2019-06-04 11:37:33 -0700

    Reimplement macro language and make test cases green.

--- cypress/integration/expectation.spec.js
+++ cypress/integration/expectation.spec.js
@@ -1,66 +1,23 @@
 /// <reference types="cypress"/>
 const _ = Cypress._
-import { expectationTable, findAllMacros, findAllVarRefs, cyGetAll } from '../../src/expectation'
+import { expectationTable } from '../../src/expectation'
 
 
-describe('define the expectation table', () => {
-  describe('findAllMacros', () => {
-   it('is not an empty array', () => {
-    let expectation = ["Array", "{@X.name}"]
- 
-    expect(findAllMacros(expectation)).to.not.be.empty
-    expect(findAllMacros(expectation)).to.be.instanceOf(Array)
-   });
- 
-   it('returns all values that meet the pattern', () => {
-    let expectation = ["not dynamic", "{@X.name}", "{@X1.first_name} {@X1.last_name}", "{@X2.last_name}, {@X2.first_name}", "{today.short}"]
-    expect(findAllMacros(expectation)).to.eql(["@X.name", "@X1.first_name", "@X1.last_name", "@X2.last_name", "@X2.first_name", "today.short"])
-   });
-  });
- 
-  describe('findAllVarRefs', () => {
-   it('handles corner cases', () => {
-     expect(findAllVarRefs([])).to.eql([])
-     expect(findAllVarRefs([])).to.eql([])
-   })
-   it('returns only the uniq variable', () => {
-    let expectation = ["@X.name", "@X1.first_name", "@X1.last_name", "@X2.last_name", "@X2.first_name", "@X2.name", "today.short"]
-    expect(findAllVarRefs(expectation)).to.eql(["@X", "@X1", "@X2"])
-   });
-  });
- 
-  describe('cyGetAll', () => {
- 
-   it('returns cypress variables in the system', () => {
-    cy.fixture('cap').as('cap')
-    cy.fixture('iron').as('iron')
-    
-    cyGetAll(["@cap", "@iron"]).then(result => {
-      expect(result).to.not.be.empty
-      expect(result['@cap']).to.include({name: 'Captain America'})
-      expect(result['@iron']).to.include({name: 'Iron Man'})
-    })
-   });
-  });
- 
- })
- 
-describe.skip('expectationTable', () => {
+describe('expectationTable', () => {
  beforeEach(function () {
-  console.log('am i run?')
-  cy.fixture('thor').storeAs('thor');
-  cy.fixture('cap').storeAs('cap');
-  cy.fixture('iron').storeAs('iron');
-  cy.fixture('date').storeAs('today');
+  cy.fixture('thor').as('thor');
+  cy.fixture('cap').as('cap');
+  cy.fixture('iron').as('iron');
+  cy.fixture('date').as('today');
  });
 
 
  it('returns the table passed in if there are no macros', () => {
-  let dataTable = [
+  const dataTable = [
    ["Name", "Weapon"],
    ["Captain America", "Shield"]
   ]
-  expect(expectationTable(dataTable)).to.eql(dataTable)
+  expectationTable(dataTable).should('eql', dataTable)
  })
 
 
@@ -73,7 +30,7 @@ describe.skip('expectationTable', () => {
    ["Name", "Weapon"],
    ["Captain America", "Shield"]
   ]
-  expect(expectationTable(dataTable)).to.eql(expected)
+  expectationTable(dataTable).should('eql', expected)  
  });
 
  it('returns multiple rows with macros', () => {
@@ -87,7 +44,7 @@ describe.skip('expectationTable', () => {
    ["Captain America", "Shield"],
    ["Iron Man", "Gauntlet"]
   ]
-  expect(expectationTable(dataTable)).to.eql(expected)
+  expectationTable(dataTable).should('eql', expected)
  })
 
  it('returns multiple rows with macros', () => {
@@ -101,19 +58,19 @@ describe.skip('expectationTable', () => {
    ["Captain America", "Shield", "1"],
    ["Iron Man", "Gauntlet", "3"]
   ]
-  expect(expectationTable(dataTable)).to.eql(expected)
+  expectationTable(dataTable).should('eql', expected)
  })
 
  it('returns date formats for today', () => {
   let dataTable = [
    ["Short", "Long", "Current Year"],
-   ["{today.short}", "{today.long}", "{today.year}"]
+   ["{$today.short}", "{$today.long}", "{$today.year}"]
   ]
   let expected = [
    ["Short", "Long", "Current Year"],
    ["6/1/19", "06/01/2019", "2019"]
   ]
-  expect(expectationTable(dataTable)).to.eql(expected)
+  expectationTable(dataTable).should('eql', expected)
  })
 
  it('returns a mixture of strings and dates', () => {
@@ -127,7 +84,7 @@ describe.skip('expectationTable', () => {
    ["Captain America", "Shield", "1", "6/1/19"],
    ["Iron Man", "Gauntlet", "3", "06/01/2019"]
   ]
-  expect(expectationTable(dataTable)).to.eql(expected)
+  expectationTable(dataTable).should('eql', expected)
  })
 
  it('replaces multiple macros in the string', () => {
@@ -139,6 +96,6 @@ describe.skip('expectationTable', () => {
    ["What is the Perferred Weapon?"],
    ["Captain America perferred is a Shield"]
   ]
-  expect(expectationTable(dataTable)).to.eql(expected)
+  expectationTable(dataTable).should('eql', expected)
  })
 })
--- src/expectation.js
+++ src/expectation.js
@@ -1,3 +1,5 @@
+import invariant from 'invariant'
+import {get as lodashGet} from 'lodash' 
 import {lex} from './lex'
 
 //TODO: Handle large Percentages for Dynamic Ownership Percentages
@@ -5,55 +7,64 @@ import {lex} from './lex'
 //TODO: Handle Dollar Amounts for Unreturned Capital
 
 //Get the Results of all the Variables from findAllVarRefs
-export function cyGetAll(names) {
+function cyGetAll(names) {
   let chain = cy;
   const values = {};
   names.forEach(name => {
     chain = chain = chain.get(name).then(value => (values[name] = value));
   });
 
-  console.log(values);
   return chain.then(() => values);
 }
 
-//In the DataTable row, find all macros used. Return them in the order in which they occur in the input strings.
-export function findAllMacros(expectation) {
-  const macros = [];
-  expectation.forEach(str => lex(str, {onMacro: t => macros.push(t)}))
-  return macros;
+// Given the first word of dot-separated macro expression, transform it into
+// a proper Cypress variable reference including the @ character.
+function canonicalize(name) {
+  switch(name.slice(0,1)) {
+    case '@':
+      return name;
+    case '$':
+      throw new Error('TODO - support macro constants')
+    default:
+      return `@${name}`
+  }
 }
 
-//In the Result of findAllMacros, then strip the variable @X part and then return the uniq set
-export function findAllVarRefs(macroContents) {
-  const uniq = {};
-  console.log('ts>', macroContents)
-  macroContents.forEach(t => {
-    const [prefix] = t.split('.', 2)
-    console.log('ts>  ', prefix)
-    if(prefix.startsWith('@')) uniq[prefix] = true
-  })
-  return Object.keys(uniq)
-}
-
-//Evaluations all the Macros and finds their cypress values lookupTable from cyGetAll
-export function evalAllMacros(expectation, lookupTable) {
-
-}
-
-//Builds the expectationTable to pass to expectTableContent
+// Given a data table (2D array), replace all macros and return a Cypress
+// chainable that resolves to the macro-substituted result (another 2D
+// array with the same dimension as dataTable).
 export function expectationTable(dataTable) {
-  var outputTable = [];
-  dataTable.forEach(expectation => {
-    cyGetAll(findAllVarRefs(findAllMacros(expectation))).then(
-      lookupTable => outputTable.push(evalAllMacros(dataTable, lookupTable))
-    )
+  //Build Out the Variables to Look For
+  const allVariables = {};
+  dataTable.forEach(expectation => 
+    expectation.forEach(cell => {
+      lex(cell, {onMacro: t => {
+        const [name] = t.split('.', 2)
+        allVariables[canonicalize(name)] = true;
+      }})
+    })
+  )
+
+  // Asynchronously resolve all distinct variables, then substitute.
+  return cyGetAll(Object.keys(allVariables)).then(lookupTable => {
+    const outputTable = [];
+
+    dataTable.forEach(expectation => {
+      const outputRow = [];
+      expectation.forEach(cell => {
+        const outputCell = []
+        lex(cell, {onText: t => outputCell.push(t), onMacro: t => {
+          const [name, path] = t.split('.', 2)
+          const object = lookupTable[canonicalize(name)]
+          invariant(object, `expectationTable: macro references unbound variable "${name}"`)
+          const substitutedValue = lodashGet(object, path)
+          outputCell.push(substitutedValue)
+        }})
+        outputRow.push(outputCell.join(''))
+      })
+      outputTable.push(outputRow)
+    })
+
+    return outputTable;
   })
-  return cy.wrap(outputTable);
 }
-
-export function replaceMacro(match) {
-  const alias = trim(match).split(".")[0];
-  const attr = trim(match).split(".")[1];
-  return 'TODO';
-}
-

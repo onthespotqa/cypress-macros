0d1f7ef02f23623f4b9d4b87b7e4677353d69942
Hillary Hueter <hillary.hueter@appfolio.com>
2019-06-06 12:36:47 -0700

    adding a cucumber parameters

--- src/parameter.js
+++ src/parameter.js
@@ -1,9 +1,13 @@
 /* global defineParameterType, then, when */
+import { canonicalize } from './expectation'
 
 defineParameterType({
  name: "macro",
- regexp: "/\{(.*?)\}/g",
+ regexp: /\{(.*?)\}/,
  transformer(s) {
-
+  let [key, value] = s.split('.', 2)
+  const name = canonicalize(key)
+  const path = value;
+  return { name: name, path: path }
  }
 })

aa1910cb493cc495ac2181257079142048dbc12d
Tony Spataro <xeger@xeger.net>
2019-06-06 20:26:27 -0700

    Freshen eslint; update package metadata.
    Remove useless lint plugins. Add VSCode format-on-save settings. Lock typescript to 3.2.2 to avoid warnings. Change package name and description.

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,34 +1,50 @@
+/**
+ * Unified ES+TS+Prettier configuration for ESLint. Inspired by:
+ * https://dev.to/robertcoopercode/using-eslint-and-prettier-in-a-typescript-project-53jb
+ *
+ * This file uses JS (not JSON) so we can have comments and object literals.
+ * PLEASE DO NOT ADD CONDITIONALS OR DYNAMIC CODE. Keep this file as pure data.
+ */
+
 module.exports = {
+  parser: '@typescript-eslint/parser',
+  plugins: ['cypress', 'prettier'],
   env: {
-    jest: true
+    browser: true,
+    es6: true
   },
   extends: [
     'eslint:recommended',
     'plugin:@typescript-eslint/recommended',
+    'plugin:cypress/recommended',
+    'prettier/@typescript-eslint',
+    // NB: please leave this at the end so it can override all other rules!
+    'plugin:prettier/recommended'
   ],
-  parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: "./tsconfig.json"
+    ecmaFeatures: {
+      jsx: true
+    },
+    ecmaVersion: 2018, // aka ECMAScript 9
+    sourceType: 'module' // allow `import` statement
   },
-  plugins: ["@typescript-eslint"],
   rules: {
-    "@typescript-eslint/no-explicit-any": "off",
-    "import/prefer-default-export": "off"
+    '@typescript-eslint/array-type': 'warn',
+    '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/class-name-casing': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/explicit-member-accessibility': 'off',
+    '@typescript-eslint/interface-name-prefix': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'no-empty': 'warn',
+    'no-extra-boolean-cast': 'warn',
   },
   overrides: [
     {
-      files: ['*.test.ts'],
-      rules: {
-        '@typescript-eslint/explicit-function-return-type': 'off',
-        '@typescript-eslint/no-object-literal-type-assertion': 'off'
+      files: ['cypress/**/*.js', 'cypress/**/*.ts'],
+      env: {
+        "cypress/globals": true
       }
     }
-  ],
-  settings: {
-    "import/resolver": {
-      node: {
-        extensions: [".js", ".jsx", ".ts", ".tsx"]
-      }
-    }
-  }
-}
+  ]
+};

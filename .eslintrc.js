9fbb004285d34a5934a0dc5d34ec15d7a6527d66
Hillary Hueter <hillary.hueter@appfolio.com>
2022-12-12 20:27:12 -0800

    run lint; change a lint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -31,6 +31,7 @@ module.exports = {
   rules: {
     '@typescript-eslint/array-type': 'warn',
     '@typescript-eslint/camelcase': 'off',
+    '@typescript-eslint/no-empty-function': 'warn',
     '@typescript-eslint/naming-convention': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-member-accessibility': 'off',
